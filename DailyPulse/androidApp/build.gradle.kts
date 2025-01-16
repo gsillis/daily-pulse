0e1bb6f1b1144e7f0f75d2b91c41a3e9abade2a8
Gabriela Sillis <gsillis@Gabrielas-MacBook-Pro.local>
2025-01-16 09:54:23 -0300

    Added: Add expect and actual Android and iOS classes

--- DailyPulse/androidApp/build.gradle.kts
+++ DailyPulse/androidApp/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.example.dailypulse.android"
-    compileSdk = 34
+    compileSdk = 35
     defaultConfig {
         applicationId = "com.example.dailypulse.android"
         minSdk = 24
